ee73b648616f8c9cb46cc668f45d01b5f093042a
Fortunetech <saniul.client@gmail.com>
2025-07-18 17:21:37 +0600

    connect to online db

--- app.js
+++ app.js
@@ -49,9 +49,8 @@ app.use(errorHandler.next);
 mongoose.set('strictQuery', false);
 mongoose
   .connect(
-    `mongodb://0.0.0.0:27017/${process.env.DB_NAME}`,
     // `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:27017/${process.env.DB_NAME}?authSource=${process.env.AUTH_SOURCE}`,
-    // `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`,
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`,
     // {
       // useNewUrlParser: true,
       //  useFindAndModify: false,
